675b9feec09fe5ac63571bea3fbda503cf535226
asoyemi1 <111797319+asoyemi1@users.noreply.github.com>
2023-08-06 15:09:23 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# LCMelt: A LC Melting Point Prediction App
+Prediction of Liquid Crystal Melting Points using Chemprop model trained on organic molecule MP data
---
+++ main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.header("LCMelt: A Liquid Crystal Melting Point Prediction App")
+st.text_input("Enter a valid SMILES string for your molecule: ", key="SMILES")
+
+if st.button('Make prediction'):
+    import chemprop
+
+    smiles = [[st.session_state.SMILES]]
+    arguments = [
+        '--test_path', '/dev/null',
+        '--preds_path', '/dev/null',
+        '--checkpoint_dir', 'Model/'
+    ]
+
+    args = chemprop.args.PredictArgs().parse_args(arguments)
+    preds = chemprop.train.make_predictions(args=args, smiles=smiles)
+    st.write(f"The melting point of your molecule is: {int(np.squeeze(preds))} degrees Celsius")
